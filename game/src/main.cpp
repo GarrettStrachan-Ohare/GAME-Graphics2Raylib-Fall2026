2a91742084dc186b75085f1f79c72fcb6ed4d79e
Garrett Strachan-Ohare <gstrachanohare@gmail.com>
2026-09-24 09:01:55 -0400

    Week3 Hello Raylib

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,18 +2,35 @@
 #include "raymath.h"
 #include "raygui.h"
 
+
 int main()
 {
     InitWindow(800, 800, "Physics-1");
     InitAudioDevice();
     SetTargetFPS(60);
 
+    float speed = 400.0f;
+    float positionX = 150.0f;
+    float positionY = 400.0f;
+
     while (!WindowShouldClose())
     {
+        // Class Lab
+		float totalTime = GetTime();
+        float frameTime =GetFrameTime();
+
+        positionX += speed * frameTime;
+
+        if (positionX > 650.0f || positionX < 150.0f)
+        {
+            speed = -speed;
+        }
+
         BeginDrawing();
         ClearBackground(WHITE);
 
         DrawCircleV(GetMousePosition(), 20.0f, RED);
+        DrawCircleV(Vector2(positionX, positionY), 15.0f, RED);
 
         EndDrawing();
     }
